f7934d8fcc4c6b57def7758a9ef298be9b375eb7
Ewiase01 <memaamesaa@gmail.com>
2023-03-30 10:33:13 -0700

    a

--- handle_print.c
+++ handle_print.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
+ *
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
@@ -9,6 +10,7 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
